6e0ca6a329b57b2106e177370f99d865ff1f0021
Adrien <adrien.hernandez@hec.ca>
2020-10-03 17:39:44 -0400

    Added 03_Data_visualization.R

---
+++ 02_Merging_Datasets.R
@@ -0,0 +1,80 @@
+# Created by Adrien Hernandez, Audrey-Anne Guindon and Mohamad Elmasri
+
+library(dplyr)
+library(tidyr)
+library(data.table)
+#install.packages("devtools")
+#devtools::install_github("melmasri/traveltimeHMM")
+library(traveltimeHMM)
+library(ggplot2)
+
+edges <- read.csv("Quebec/edges.csv")
+vehrouteData <- read.csv("Quebec/vehroutedata_cleaned.csv")
+
+## Format edges and keep edge ID and edge length
+edges_file <- function(edges){
+  edges %>%
+  mutate(
+    edge = gsub(":", "", edge_id),
+  ) %>%
+  select(edge, length)%>%
+  arrange(edge, length) %>%
+  group_by(edge)%>%
+  summarise(length = first(length))
+}
+
+edges <- edges_file(edges)
+
+# Cleaning \t in vehroudata
+
+## Join edges to trip data to obtain edge length and rename columns
+join_edges2trip <-function(data){
+  data$edge <- gsub("\t", "", data$edge)
+  vehrouteData %>%
+  left_join(edges, by ="edge") %>%
+  arrange(id, time) %>%
+  mutate(
+    traveltime = ifelse(tt==0, 0.5, tt),
+    tripID = id,
+    linkID = gsub("\t", "", edge),
+    logspeed = log(length/traveltime),
+    timeBin = 0,
+    speedkmh = (length/traveltime)*3.6,
+    time = gsub("\t", "", time)
+  ) %>%
+  select(tripID, linkID, timeBin, logspeed, speedkmh, traveltime, length, time)
+}  
+
+Sumo_data <- join_edges2trip(vehrouteData)
+
+## Add time bins
+rules = list(
+  #list(start='7:00', end= '10:00', days = 0:6, tag='MR'),
+  #list(start='12:00', end='15:00', days = 0:6, tag='NR'),
+  #list(start='17:00', end= '20:00', days = 0:6, tag='ER')
+  list(start='6:30', end= '9:00', days = 0:6, tag='MR'),
+  list(start='15:00', end= '18:00', days = 0:6, tag='ER')
+)
+time_bins <- rules2timebins(rules)
+Sumo_data$timeBin = time_bins(Sumo_data$time)
+
+# Creating column speed
+Sumo_data$speed <- exp(Sumo_data$logspeed)
+
+# We remove trips whose one of the link speed is > 150
+Sumo_data <- as.data.table(Sumo_data)
+Sumo_data[, whour :=as.POSIXlt(time)$wday*24 + as.POSIXlt(time)$hour]
+Sumo_data[, week :=week(time)]
+r = Sumo_data[3.6*exp(logspeed)>150][, tripID[1],tripID][, tripID]
+Sumo_data= Sumo_data[!tripID %in% r][order(tripID, time)]
+
+# We remove the last link from each trip because the cars never end up their trip at the same time
+#Sumo_data = Sumo_data[,.SD[-.N] , tripID]
+#Sumo_data = Sumo_data[,.SD[-1] , tripID]
+Sumo_data = Sumo_data[order(tripID, time)]
+
+# We rename the columns to get the correct names for our method
+Sumo_data <- as.data.frame(Sumo_data)
+Sumo_data <- rename(Sumo_data, c("trip"="tripID", "timeBins"="timeBin", "linkId"="linkID", "tt"="traveltime"))
+
+write.csv(Sumo_data, "Quebec/Sumo_data.csv")
---
+++ 03_Data_Visualization.R
@@ -0,0 +1,28 @@
+# Created by Adrien Hernandez
+
+library(ggplot2)
+library(splitstackshape)
+
+Sumo_data <- read.csv("Quebec/Sumo_data.csv")
+
+# Visualization of the traffic density over 24h
+
+Sumo_data_time <- data.frame(Data=as.POSIXct(Sumo_data$time, format="%Y-%m-%d %H:%M:%S"))
+
+ggplot(Sumo_data_time, aes(x=Data)) + 
+  geom_histogram(binwidth=100, fill="grey", colour="black") + #binwidth in seconds
+  ylab("Number of trips") + #xlab should pick up variable name 'Data'
+  ggtitle("Traffic on 24h")
+
+# Visualization of the variation of the speed for a sample of trip
+Sumo_data <- getanID(data = Sumo_data, id.vars = "trip")
+set.seed(2)
+sample_plot <- unique(Sumo_data$trip[Sumo_data$timeBins=="MR"])
+sample_plot <- sample(sample_plot, 5)
+Sumo_data_sample_plot <- Sumo_data[Sumo_data$trip %in% sample_plot]
+Sumo_data_sample_plot$trip <- as.factor(Sumo_data_sample_plot$trip)
+Sumo_data_sample_plot <- rename(Sumo_data_sample_plot, c("nbr_edge" = ".id"))
+
+
+ggplot(Sumo_data_sample_plot, aes(x = Sumo_data_sample_plot$nbr_edge, y = Sumo_data_sample_plot$speedkmh, group = Sumo_data_sample_plot$trip, color = Sumo_data_sample_plot$trip)) + geom_line() + geom_point() + scale_colour_discrete(drop=TRUE,
+                                                                                                                                                                                                                                        limits = levels(Sumo_data_sample_plot$trip))
