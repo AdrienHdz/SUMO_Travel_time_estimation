7664c29992b356df4f56ee7b8ac3857cf25bd813
Adrien <adrien.hernandez@hec.ca>
2020-10-04 19:59:35 -0400

    added 06_Final_Applied_Algorithm.R

--- 05_Algorithm.R
+++ 05_Algorithm.R
@@ -1,23 +1,13 @@
 # Created by Adrien Hernandez and Mohamad Elmasri
 
-library(traveltimeHMM)
-library(dplyr)
-library(tidyr)
-library(data.table)
 
-rules = list(
-  list(start='6:30', end= '9:00', days = 0:6, tag='MR'),
-  list(start='15:00', end= '18:00', days = 0:6, tag='ER')
-)
-
-traveltimeCLT <- function(data_train = NULL, l = 2, M = 1000, data_TimeBins = c("MR", "ER", "Other"), bin = "MR", tb_rules = rules){
+traveltimeCLT <- function(data_train = NULL, L = NULL, M = NULL, data_TimeBins = NULL, bin = NULL, rules = NULL){
   
   # Creating column linkId.from, linkId.to and N
   data_train[, linkId.from := linkId, by = trip]
   data_train[, linkId.to := shift(linkId, type = 'lead'), by = trip]
   data_train[, N:=.N, by =list(linkId.from, linkId.to, timeBins) ]
   
-  L = l  # minimum number of observations
   graph.stat = data_train[, .(mu = mean(1/speed),  sd2 = var(1/speed), N=N[1]) ,by = list(linkId.from, linkId.to, timeBins)]
   
   full.graph = graph.stat[, .(timeBins = data_TimeBins) ,by = list(linkId.from, linkId.to)]
@@ -87,9 +77,8 @@ traveltimeCLT <- function(data_train = NULL, l = 2, M = 1000, data_TimeBins = c(
     # Algorithm
     
   param_zeta<-function(t0, rho,linkfrom, linkto, len, sequence = FALSE){
-    tb_rules = rules
     t0 = as.POSIXlt(t0)
-    time_bins <- rules2timebins(tb_rules)
+    time_bins <- rules2timebins(rules)
     tbin = time_bins(t0)
     g = graph.stat.full[linkId.from %in% linkfrom]
     t = 0                               # time (mean)
@@ -139,17 +128,22 @@ traveltimeCLT <- function(data_train = NULL, l = 2, M = 1000, data_TimeBins = c(
     } else{
     A = data_train[order(trip, time)]}
   
-    B = A[, param_zeta(time[1], rho, linkId.from, linkId.to, length), by = trip]
-    D = merge(B, A[, .(obstt= sum(tt)), trip])
-    D[, res := (obstt - tt)/sd]
-    res = list(db = D, res.sd = sd(D$res))
-    v = res$res.sd
-    print("This is the mean of the residuals")
-    print(v)
-    return(v)
+  B = A[, param_zeta(time[1], rho, linkId.from, linkId.to, length), by = trip]
+  D = merge(B, A[, .(obstt= sum(tt)), trip])
+  D[, res := (obstt - tt)/sd]
+  res = list(db = D, res.sd = sd(D$res))
+  v = res$res.sd
+  print("This is the mean of the residuals")
+  print(v)
+  
+  obj <- list(Variance = v, Graph.stat = graph.stat.full, Rho = rho)
+  
+  class(obj) = append(class(obj), "traveltime", after=0)
+  
+  invisible(obj)
 }
   
-predict.traveltimeCLT <- function(data_test = NULL, bin = "MR"){
+predict.traveltimeCLT <- function(traveltimeCLTobject = NULL, data_test = NULL, bin = "MR", rules = myrules){
   
   data_test[, linkId.from := linkId, by = trip]
   data_test[, linkId.to := shift(linkId, type = 'lead'), by = trip]
@@ -159,9 +153,53 @@ predict.traveltimeCLT <- function(data_test = NULL, bin = "MR"){
   # We take trips from the test set that occured in the morning
   samp_test = data_test[, timeBins[1], trip][V1 == bin , trip]
   
+  param_zeta<-function(t0, rho,linkfrom, linkto, len, sequence = FALSE){
+    t0 = as.POSIXlt(t0)
+    time_bins <- rules2timebins(rules)
+    tbin = time_bins(t0)
+    g = traveltimeCLTobject$Graph.stat[linkId.from %in% linkfrom]
+    t = 0                               # time (mean)
+    vcumlative = 0                                 # variance
+    vprev = 1
+    lenprev = 1
+    if(sequence){
+      t.seq = v.seq = numeric(length(len))
+    }
+    for(i  in 1:(length(len) - 1)){
+      ##for(i in 1:81){
+      d = g[linkId.from == linkfrom[i] & linkId.to == linkto[i] & timeBins == tbin]
+      t = t + len[i]*d$mean
+      tbin = time_bins(t0 + t)
+      vcumlative = vcumlative + len[i]^2  * d$sd^2  + 2*rho* len[i]*lenprev * d$sd * vprev
+      vprev = d$sd
+      lenprev = len[i]
+      if(sequence){
+        v.seq[i] = vcumlative
+        t.seq[i] = t
+      }
+    }
+    ## last linkId.to is NA
+    i = i+1
+    if(is.na(linkto[i])){
+      d = g[linkId.from == linkfrom[i] & timeBins == tbin & is.na(linkId.to)]
+    }else{
+      d = g[linkId.from == linkfrom[i] & linkId.to == linkto[i] & timeBins == tbin]
+    }
+    t = t + len[i]*d$mean
+    vcumlative = vcumlative + len[i]^2 * d$sd^2 + 2* rho * len[i]*lenprev *d$sd *vprev
+    if(sequence){
+      v.seq[i] = vcumlative
+      t.seq[i] = t
+    }
+    s = sqrt(vcumlative)
+    if(sequence){
+      list(tt =t, sd = s, v.seq = sqrt(v.seq), t.seq = t.seq)
+    }else{
+      list(tt =t, sd = s)
+    }}
   
   # We apply the method on the test data
-  A = data_test[trip %in% samp_test, param_zeta(time[1], rho, linkId.from, linkId.to, length), by = trip]
+  A = data_test[trip %in% samp_test, param_zeta(time[1], traveltimeCLTobject$Rho, linkId.from, linkId.to, length), by = trip]
   
   # We merge to be able to get the observed travel time, the real travel time, and the variance
   B = merge(data_test[, .(obstt = sum(tt)), trip], A)
@@ -182,11 +220,40 @@ predict.traveltimeCLT <- function(data_test = NULL, bin = "MR"){
   }
   
   print("Model estimation using V")
-  print(t(B[ , numerical_res(obstt, tt, sd *qnorm(0.975)*v)]))
+  print(t(B[ , numerical_res(obstt, tt, sd *qnorm(0.975)*traveltimeCLTobject$Variance)]))
   # We get the results without using v
   print("Model estimation without V")
   print(t(B[ , numerical_res(obstt, tt, sd *qnorm(0.975))]))
   
 }
 
-montoto <- traveltimeCLT(data_train = train)
+create_test_trips<-function(M = 500, db, min.n = 1){
+  ## Setting the graph
+  db[, linkId.from := linkId, by = trip]
+  db[, linkId.to := shift(linkId, type = 'lead'), by = trip]
+  db[, N:=.N, by =list(linkId.from, linkId.to, timeBins) ]
+  A = db[, .(N = N[1]), by =list(linkId.from, linkId.to, timeBins) ]
+  A[, N.left := 0 ] 
+  setkey(A, linkId.from, linkId.to, timeBins)
+  
+  bank = c()
+  unique.trips =db[, min(N>1)  , trip][V1 ==1][, trip]
+  
+  repeat{
+    n = length(unique.trips)
+    car = unique.trips[sample.int(n,1)]
+    k = db[trip==car, .(linkId.from, linkId.to, timeBins)]
+    
+    if(A[k, all(N -N.left - 1> 0 )]){
+      bank = c(bank,car)              ## add the trip
+      unique.trips = setdiff(unique.trips, bank) ## removing all smapled trips
+      ## updating A
+      A[k, N.left:= N.left + 1]
+      ## break if number of samples reached
+      if(length(bank) >=M)
+        break
+    }
+    
+  }
+  bank
+}
---
+++ 06_Final_Applied_Algorithm.R
@@ -0,0 +1,49 @@
+# Created by Adrien Hernandez
+
+#install.packages("devtools")
+#devtools::install_github("melmasri/traveltimeHMM")
+library(traveltimeHMM)
+library(dplyr)
+library(tidyr)
+library(data.table)
+source('05_Algorithm.R')
+
+
+# Loading data
+Sumo_data <- read.csv("Quebec/Real_data.csv")
+Sumo_data$speed <- exp(Sumo_data$logspeed)
+
+# Transforming variables
+Sumo_data <- as.data.table(Sumo_data)
+Sumo_data$timeBins <- as.character(Sumo_data$timeBins)
+
+# Creating test trips
+set.seed(2020)
+test.trips.Sumo <- create_test_trips(M = 2000, Sumo_data, min.n = 1)
+
+# Splitting data into train and test set
+test = Sumo_data[trip %in% test.trips.Sumo]
+train = Sumo_data[!trip %in% test.trips.Sumo]
+
+# Counting the the number of trips in each set
+print(paste0("Number of trips inside the test set: ", test[, 1, trip][, sum(V1)]))
+print(paste0("Number of trips inside the train set: ", train[, 1, trip][, sum(V1)]))
+print(paste0("Number of trips in total: ", test[, 1, trip][, sum(V1)] + train[, 1, trip][, sum(V1)]))
+
+
+# Setting up the rules for our dataset
+myrules = list(
+  list(start='6:30', end= '9:00', days = 0:6, tag='MR'),
+  list(start='15:00', end= '18:00', days = 0:6, tag='ER')
+)
+
+mytimebins = c("MR", "ER", "Other")
+
+
+# We run the travel time estimation method
+ttCLTmodel <- traveltimeCLT(data_train= train, L = 2, M = 1000, data_TimeBins = mytimebins,
+                            bin = "MR", rules = myrules)
+
+ttCLTresults <- predict.traveltimeCLT(traveltimeCLTobject = ttCLTmodel, data_test = test,
+                                      bin = "MR", rules = myrules)
+
